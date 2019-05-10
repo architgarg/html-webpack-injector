e7bed3492252b6eae23690a0f434dc545b406269
Archit Garg <architgarg1997@gmail.com>
2019-05-11 04:46:32 +0530

    updated to support latest version

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 class HtmlWebpackInjectorPlugin {
     apply(compiler) {
-        compiler.plugin("compilation", compilation => {
-            compilation.plugin("html-webpack-plugin-alter-asset-tags", object => {
-                const chunks = [...object.head, ...object.body];
-                const headChunks = [];
-                const bodyChunks = [];
+        compiler.hooks.compilation.tap('HtmlWebpackInjectorPlugin', (compilation) => {
+            HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
+              'HtmlWebpackInjectorPlugin',
+              (data, callback) => {
+                  const chunks = [...data.headTags, ...data.bodyTags];
+                  const headChunks = [];
+                  const bodyChunks = [];
 
-                chunks.forEach(chunk => {
-                    if (chunk.attributes.src.includes("_head")) {
-                        headChunks.push(chunk);
-                    } else {
-                        bodyChunks.push(chunk);
-                    }
-                });
+                  chunks.forEach(chunk => {
+                      if (chunk.attributes.src.includes("_head")) {
+                          headChunks.push(chunk);
+                      } else {
+                          bodyChunks.push(chunk);
+                      }
+                  });
 
-                object.head = headChunks;
-                object.body = bodyChunks;
-            });
-        });
+                  data.headTags = headChunks;
+                  data.bodyTags = bodyChunks;
+
+                  callback(null, data)
+              }
+            )
+        })
     }
 }
 
