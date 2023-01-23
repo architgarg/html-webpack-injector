f716e496e95077e37d01e9a48ec4f8b5578a9d25
tech-meppem <luke.meppem@netduma.com>
2023-01-23 14:59:49 +0000

    fixed & improved d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,19 @@
-import { type WebpackPluginInstance } from 'webpack';
+import { Compiler, WebpackPluginInstance } from 'webpack';
+import { } from 'html-webpack-plugin';
 
-declare module "html-webpack-plugin" {
-  declare namespace HtmlWebpackPlugin {
-    interface Options {
-      chunksConfig: {
-        async: string[],
-        defer: string[],
-        removeDefer: string[],
-      }
-    }
+declare module 'html-webpack-plugin' {
+  export interface Options extends InjectorOptions { }
+}
+
+export interface InjectorOptions {
+  chunksConfig?: {
+    async?: string[],
+    defer?: string[],
+    removeDefer?: string[],
   }
 }
 
-class HtmlWebpackInjectorPlugin implements WebpackPluginInstance { }
-
-module.exports = HtmlWebpackInjectorPlugin;
+export default class HtmlWebpackInjectorPlugin implements WebpackPluginInstance {
+  [index: string]: any;
+  apply: (compiler: Compiler) => void;
+}
